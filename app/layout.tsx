5eef0489294b44ed38db075fce107fa4a5c19846
Zenovs <zenovs@users.noreply.github.com>
2026-03-06 07:34:07 +0000

    Apply darknet-inspired design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'CoreMail - E-Mail Marketing Tool',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body className={inter.className}>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
